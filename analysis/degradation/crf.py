ca033f1da23eb6aa29bc68e45285c9d5dc5438fc
AngaBlue <contact@anga.blue>
2025-09-14 21:16:59 +1000

    fix: simplify CRF

--- analysis/degradation/crf.py
+++ analysis/degradation/crf.py
@@ -1,17 +1,14 @@
-from math import floor
 import os
 import subprocess
 from pathlib import Path
 from typing import Generator, Tuple
-from utils.video_io import read_video
 
 CRF_LEVELS = [25, 30, 35, 40, 45, 51]
-GOP = 10  # seconds
 
 
 def apply(input_path: str) -> Generator[Tuple[str, str], None, None]:
     """
-    Applies temporal compression to the video by varying H264 CRF.
+    Applies temporal compression to the video by varying H.264 CRF.
     """
     base_name = Path(input_path).stem
     output_root = Path("results") / base_name / "degraded" / "crf"
@@ -24,11 +21,12 @@ def apply(input_path: str) -> Generator[Tuple[str, str], None, None]:
         out_path = output_root / f"{crf}.mp4"
         if not out_path.exists():
             cmd = [
-                "ffmpeg", "-i", input_path,
+                "ffmpeg",
+                "-hide_banner",
+                "-loglevel", "error",
+                "-i", input_path,
                 "-c:v", "libx264",
                 "-crf", str(crf),
-                # fixed B-frames (constant across outputs)
-                "-bf", "4",
                 "-pix_fmt", "yuv420p",
                 str(out_path)
             ]
